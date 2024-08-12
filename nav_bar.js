e82a7fe8d928e0250b81c67cbe6d52b578aa01b5
FARHAN AHMAD <113082273+farahn0104@users.noreply.github.com>
2024-08-12 08:11:26 +0530

    Delete nav_bar.js

--- nav_bar.js
+++
@@ -1,8 +0,0 @@
-// JavaScript for mobile menu toggle
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const navList = document.getElementById('nav-list');
-    const menuToggle = document.getElementById('menu-toggle');
-
-    navList.classList.toggle('active');
-    menuToggle.classList.toggle('active');
-});
